ab2f1c8c1bae548585b9f363c6459d8da0d50b50
henryfrstr <henryfrstr@gmail.com>
2021-11-05 04:06:09 +0300

    add city creation on db

--- src/weather_app/views.py
+++ src/weather_app/views.py
@@ -1,15 +1,35 @@
+from os import name
 from django.shortcuts import render
 import requests
 from decouple import config
 from pprint import pprint
 from .models import City
+from django.contrib import messages
 
 
 def home(request):
     cities = City.objects.all()
-    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}"
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric"
     # city = "İstanbul"
     # response = requests.get(url.format(city, config("API_KEY"))).json()
+    a = {
+        "a": "henry"
+    }
+    g_city = request.GET.get('city')  # GET/POST/DELETE/PUT
+    print("g_city: ", g_city)
+    if g_city:
+        response = requests.get(url.format(g_city, config("API_KEY")))
+        print(response.status_code)
+        if response.status_code == 200:
+            content = response.json()
+            a_city = content['name']
+            if City.objects.filter(name=a_city):
+                messages.warning(request, "City already exists.")
+            else:
+                City.objects.create(name=a_city)
+                messages.success(request, "City succesfully created.")
+        else:
+            messages.warning(request, "City does not exits.")
     city_data = []
     for city in cities:
         # print(city)
@@ -23,7 +43,7 @@ def home(request):
             "icon": response['weather'][0]['icon']
         }
         city_data.append(data)
-    print(city_data)
+    # print(city_data)
     context = {
         "city_data": city_data
     }
